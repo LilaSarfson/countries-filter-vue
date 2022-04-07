7a40d92106d24cf356ec179872576bf5895551f7
LilaSarfson <lilamandarina@hotmail.com>
2022-04-07 13:38:25 +0200

    half done

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 // Suerte :)
+Vue.createApp({
+    data() {
+        return {
+            countries: [],
+            textInput: "",
+            regionCountryFilter: "",
+
+        }
+    },
+
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        async fetchData() {
+            const response = await fetch("https://restcountries.com/v2/all");
+            this.countries = await response.json();
+            console.log(this.countries);
+        }
+    },
+    computed: {
+        searchCountry() {
+            if (this.textInput) {
+                let countrySearched = this.countries.filter((x) => x.name.includes(this.textInput));
+                return countrySearched;
+            }
+
+            if (this.regionCountryFilter) {
+                let IwantaRegion = this.countries.filter((x) => x.region.includes(this.regionCountryFilter));
+                return IwantaRegion;
+            }
+
+            else if (this.textInput && this.regionCountryFilter)
+                return this.countries
+        }
+    },
+
+
+}).mount('#app')
+
